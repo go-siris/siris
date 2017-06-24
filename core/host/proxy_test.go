08878811de24c422ed0bc4d4256f4893b35bad90
Josef Fröhle <github@josef-froehle.de>
2017-06-24 19:46:58 +0200

    Fix routing check also against isDynamic
    fix for wrong "static resource don't limit root=true and child = nil"

--- core/host/proxy_test.go
+++ core/host/proxy_test.go
@@ -4,6 +4,7 @@ package host_test
 import (
 	"net"
 	"net/url"
+	"strconv"
 	"testing"
 
 	"github.com/go-siris/siris"
@@ -15,6 +16,9 @@ import (
 func TestProxy(t *testing.T) {
 	expectedIndex := "ok /"
 	expectedAbout := "ok /about"
+	expectedDemo := "ok /name/demo"
+	expectedDemoId := "ok /id/1234"
+	expectedDemoIdMin := "ok /min/1234"
 	unexpectedRoute := "unexpected"
 
 	// proxySrv := siris.New()
@@ -34,12 +38,40 @@ func TestProxy(t *testing.T) {
 	go host.NewProxy("localhost:2017", u).ListenAndServe() // should be localhost/127.0.0.1:80 but travis throws permission denied.
 
 	app := siris.New()
+
+	app.Macros().Int.RegisterFunc("min", func(minValue int) func(string) bool {
+		// do anything before serve here [...]
+		// at this case we don't need to do anything
+		return func(paramValue string) bool {
+			n, err := strconv.Atoi(paramValue)
+			if err != nil {
+				return false
+			}
+			return n >= minValue
+		}
+	})
+
 	app.Get("/", func(ctx context.Context) {
 		ctx.WriteString(expectedIndex)
 	})
 
 	app.Get("/about", func(ctx context.Context) {
 		ctx.WriteString(expectedAbout)
+	})
+
+	app.Get("/name/{name}", func(ctx context.Context) {
+		name := ctx.Params().Get("name")
+		ctx.WriteString("ok /name/" + name)
+	})
+
+	app.Get("/id/{id:int}", func(ctx context.Context) {
+		id, _ := ctx.Params().GetInt("id")
+		ctx.WriteString("ok /id/" + strconv.Itoa(id))
+	})
+
+	app.Get("/min/{id:int min(1000)}", func(ctx context.Context) {
+		id, _ := ctx.Params().GetInt("id")
+		ctx.WriteString("ok /min/" + strconv.Itoa(id))
 	})
 
 	app.OnErrorCode(siris.StatusNotFound, func(ctx context.Context) {
@@ -56,5 +88,11 @@ func TestProxy(t *testing.T) {
 	e := httptest.NewInsecure(t, httptest.URL("http://localhost:2017"))
 	e.GET("/").Expect().Status(siris.StatusOK).Body().Equal(expectedIndex)
 	e.GET("/about").Expect().Status(siris.StatusOK).Body().Equal(expectedAbout)
+	e.GET("/name/demo").Expect().Status(siris.StatusOK).Body().Equal(expectedDemo)
+	e.GET("/id/1234").Expect().Status(siris.StatusOK).Body().Equal(expectedDemoId)
+	e.GET("/id/notfound").Expect().Status(siris.StatusNotFound).Body().Equal(unexpectedRoute)
+	e.GET("/min/1234").Expect().Status(siris.StatusOK).Body().Equal(expectedDemoIdMin)
+	e.GET("/min/123").Expect().Status(siris.StatusNotFound).Body().Equal(unexpectedRoute)
+	e.GET("/min/notfound").Expect().Status(siris.StatusNotFound).Body().Equal(unexpectedRoute)
 	e.GET("/notfound").Expect().Status(siris.StatusNotFound).Body().Equal(unexpectedRoute)
 }
