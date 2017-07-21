814aea098ad10ee1e0b1ed33f40c838d214a6b12
Josef Fröhle <github@josef-froehle.de>
2017-07-21 08:52:51 +0200

    Change Docs and Example add also the SessionRegenerateID function to the Context to regenerate a new SessionID.

--- _examples/intermediate/sessions/flash-messages/main.go
+++ _examples/intermediate/sessions/flash-messages/main.go
@@ -10,29 +10,35 @@ func main() {
 	app := siris.New()
 	// output startup banner and error logs on os.Stdout
 
-	sess := sessions.New(sessions.Config{Cookie: "myappsessionid"})
-	app.AttachSessionManager(sess)
+	app.AttachSessionManager("memory", &sessions.ManagerConfig{
+		CookieName:      "go-session-id",
+		EnableSetCookie: true,
+		Gclifetime:      3600,
+		Maxlifetime:     7200,
+	})
 
 	app.Get("/set", func(ctx context.Context) {
-		ctx.Session().SetFlash("name", "siris")
+		ctx.Session().Set("name", "siris")
 		ctx.Writef("Message set, is available for the next request")
 	})
 
 	app.Get("/get", func(ctx context.Context) {
-		name := ctx.Session().GetFlashString("name")
+		name := ctx.Session().Get("name")
 		if name == "" {
 			ctx.Writef("Empty name!!")
 			return
 		}
+		defer ctx.Session().Delete("name")
 		ctx.Writef("Hello %s", name)
 	})
 
 	app.Get("/test", func(ctx context.Context) {
-		name := ctx.Session().GetFlashString("name")
+		name := ctx.Session().Get("name")
 		if name == "" {
 			ctx.Writef("Empty name!!")
 			return
 		}
+		defer ctx.Session().Delete("name")
 
 		ctx.Writef("Ok you are comming from /set ,the value of the name is %s", name)
 		ctx.Writef(", and again from the same context: %s", name)
