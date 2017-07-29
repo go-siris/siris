d5e76bc7aaafe9d8e82e77e1ac14b0cc019195aa
Josef Fröhle <github@josef-froehle.de>
2017-07-29 19:07:30 +0200

    Update Vendor

---
+++ vendor/github.com/lucas-clemente/quic-go/quic_suite_test.go
@@ -0,0 +1,24 @@
+package quic
+
+import (
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestQuicGo(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "QUIC Suite")
+}
+
+var mockCtrl *gomock.Controller
+
+var _ = BeforeEach(func() {
+	mockCtrl = gomock.NewController(GinkgoT())
+})
+
+var _ = AfterEach(func() {
+	mockCtrl.Finish()
+})
